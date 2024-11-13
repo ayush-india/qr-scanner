3e1cafc565b00ebaa92f53550885817f57047042
ayush mishra <mail81mitthu@gmail.com>
2024-11-13 15:34:50 +0530

    a: barcode detection completed
    now it can dected bar code from an image and tell its details, will work
    on if its a link then open it in chrome :)

--- barcode_scanner.py
+++ barcode_scanner.py
@@ -10,8 +10,24 @@ ap.add_argument("-i",  "--image", required=True,
 arg = vars(ap.parse_args())
 
 # load the image to memory
-
 image = cv2.imread(arg["image"])
 
 #decode the bars in image
 barcodes = pyzbar.decode(image)
+
+for barcode in barcodes:
+
+    (x,y,w,h) = barcode.rect
+    cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,225), 2)
+
+    barcodeData = barcode.data.decode("utf-8")
+    barcodeType = barcode.type
+
+    text = "{} ({})".format(barcodeData, barcodeType)
+    cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5,
+                (0, 0, 225), 2)
+
+    print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+
+cv2.imshow("Img", image)
+cv2.waitKey(0)
